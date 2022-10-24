ed2e7b702e1a5586a401a35579aa49b2779f798e
Ivan <54203256+ushigb@users.noreply.github.com>
2022-10-24 17:43:41 +0300

    Add files via upload

---
+++ contactForm/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <?php
+    mb_internal_encoding('UTF-8');
+    ?>
+    <head>
+        <meta charset="utf-8">
+        <title>Форма за контакти</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="contact-form.css?v=<?= time(); ?>"/>
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+              integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+              crossorigin="anonymous">
+    </head>  
---
+++ contactForm/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'functions.php';
+include 'header.php';
+include 'body.php';
+include 'footer.php';
+
+
+
